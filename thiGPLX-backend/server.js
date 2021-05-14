b455b2a447f57635ae63e0b53467a88990fa0c94
ShShee <55185282+ShShee@users.noreply.github.com>
2021-05-14 14:32:39 +0700

    Added a Procfile.

--- thiGPLX-backend/server.js
+++ thiGPLX-backend/server.js
@@ -1,7 +1,7 @@
 // Require packages and set the port
 const express = require("express");
 const routes = require("./routes");
-const port = 3002;
+const port = 5000;
 const bodyParser = require("body-parser");
 const app = express();
 
